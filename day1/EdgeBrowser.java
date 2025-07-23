e8d0286cc86287b8f1064aaf12b1c4f141682e86
karthikayini-123 <karthikayinibm@gmail.com>
2025-07-23 13:48:32 +0530

    Add files via upload

--- day1/EdgeBrowser.java
+++ day1/EdgeBrowser.java
@@ -4,10 +4,11 @@ public class EdgeBrowser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Browser openBrowser = new Browser();
-		String gchrome = openBrowser.launchBrowser();
-		System.out.println(gchrome);
-		openBrowser.loadUrl();
+		
+		  Browser openBrowser = new Browser(); String gchrome =
+		  openBrowser.launchBrowser(); System.out.println(gchrome);
+		  openBrowser.loadUrl();
+		 
 	}
 
 }
