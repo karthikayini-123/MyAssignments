0d8ab9f64f1013e42a82af44070579ba8bbe2ff6
karthikayini-123 <karthikayinibm@gmail.com>
2025-08-06 19:51:01 +0530

    Delete runner directory

--- runner/RunnerClass.java
+++
@@ -1,9 +0,0 @@
-package runner;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/main/java/features/Login1.feature",glue="stepdefinition")
-public class RunnerClass extends AbstractTestNGCucumberTests {
-	
-}
